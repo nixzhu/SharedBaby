06cc7d15259a6f7b391700d528fcdaf013b4e5a7
nixzhu <zhuhongxu@gmail.com>
2017-04-19 10:14:36 +0800

    index

---
+++ Sources/App/Controllers/IndexController.swift
@@ -0,0 +1,39 @@
+import Foundation
+import Vapor
+import HTTP
+import BabyBrain
+
+final class IndexController {
+
+    func addRoutes(drop: Droplet) {
+        drop.get("/", handler: index)
+    }
+
+    func index(request: Request) throws -> ResponseRepresentable {
+        func string(forKey key: String) -> String? {
+            if let string = request.query?[key]?.string {
+                return string.isEmpty ? nil : string
+            } else {
+                return nil
+            }
+        }
+        let jsonString = string(forKey: "json_string") ?? ""
+        let modelName = string(forKey: "model_name") ?? "Model"
+        let ouputModel: String
+        if let (value, _) = parse(jsonString) {
+            let upgradedValue = value.upgraded(newName: modelName)
+            ouputModel = upgradedValue.swiftStructCode()
+        } else {
+            ouputModel = ""
+        }
+        print(ouputModel)
+        return try drop.view.make("index", [
+            "jsonString": jsonString,
+            "modelName": modelName,
+            "model": ouputModel,
+            "hidden": ouputModel.isEmpty ? "hidden" : "xxx",
+            "date": "\(Date())"
+            ]
+        )
+    }
+}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,21 +1,8 @@
 import Vapor
-import BabyBrain
 
 let drop = Droplet()
 
-drop.get { req in
-    let jsonString = "{}"
-    if let (value, _) = parse(jsonString) {
-        let upgradedValue = value.upgraded(newName: "Model")
-        print(upgradedValue.swiftStructCode())
-    } else {
-        print("Invalid JSON!")
-    }
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
-}
-
-drop.resource("posts", PostController())
+let index = IndexController()
+index.addRoutes(drop: drop)
 
 drop.run()
