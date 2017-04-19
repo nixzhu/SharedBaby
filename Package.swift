957fe430592e76cde32f04cf3469889a0375e671
nixzhu <zhuhongxu@gmail.com>
2017-04-19 09:58:18 +0800

    init

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SharedBaby",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/nixzhu/Baby.git", majorVersion: 0, minor: 2)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,8 +1,16 @@
 import Vapor
+import BabyBrain
 
 let drop = Droplet()
 
 drop.get { req in
+    let jsonString = "{}"
+    if let (value, _) = parse(jsonString) {
+        let upgradedValue = value.upgraded(newName: "Model")
+        print(upgradedValue.swiftStructCode())
+    } else {
+        print("Invalid JSON!")
+    }
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
