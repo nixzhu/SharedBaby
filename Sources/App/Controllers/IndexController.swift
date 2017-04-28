9fade8346a3e9bfb8b95cfe0211fd86d686f4475
nixzhu <zhuhongxu@gmail.com>
2017-04-28 16:31:18 +0800

    better default jsonString

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -17,7 +17,7 @@ final class IndexController {
                 return nil
             }
         }
-        let jsonString = string(forKey: "json_string") ?? "{\n  \"name\": \"NIX\",\n  \"age\": 18\n}"
+        let jsonString = string(forKey: "json_string") ?? "{\n  \"name\": \"NIX\",\n  \"age\": 18, \n  \"detail\": {\n    \"is_dog_lover\": true,\n    \"skills\": [\n      {\n      \"language\": \"Swift\",\n      \"platform\": \"iOS\"\n      },\n      {\n      \"language\": \"C\",\n      \"platform\": null\n      }\n    ]\n  }\n}"
         let modelName = string(forKey: "model_name") ?? "Model"
         let isPublic = string(forKey: "isPublic") == "on"
         let modelType = string(forKey: "modelType") ?? "struct"
@@ -26,13 +26,14 @@ final class IndexController {
         let ouputModel: String
         if let (value, _) = parse(jsonString) {
             let upgradedValue = value.upgraded(newName: modelName)
+            let indentation = Indentation(level: 0, unit: "    ")
             let meta = Meta(
                 isPublic: isPublic,
                 modelType: modelType,
                 declareVariableProperties: declareVariableProperties,
                 jsonDictionaryName: jsonDictionaryName
             )
-            ouputModel = upgradedValue.swiftStructCode(meta: meta)
+            ouputModel = upgradedValue.swiftStructCode(indentation: indentation, meta: meta)
         } else {
             ouputModel = jsonString.isEmpty ? "" : "Invalid JSON!"
         }
