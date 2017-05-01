21e45fdd70fdb16c03116b8663b8ed6bef566ca2
nixzhu <zhuhongxu@gmail.com>
2017-05-01 10:10:36 +0800

    propertyMap

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -23,6 +23,14 @@ final class IndexController {
         let modelType = string(forKey: "modelType") ?? "struct"
         let declareVariableProperties = string(forKey: "isVariable") == "on"
         let jsonDictionaryName = string(forKey: "json_dictionary_name") ?? "[String: Any]"
+        let propertyMapString = string(forKey: "property_map") ?? "is_dog_lover:likeDogs,lang:language"
+        var propertyMap: [String: String] = [:]
+        propertyMapString.components(separatedBy: ",").forEach {
+            let parts = $0.components(separatedBy: ":")
+            if parts.count == 2 {
+                propertyMap[parts[0]] = parts[1]
+            }
+        }
         let ouputModel: String
         if let (value, _) = parse(jsonString) {
             let upgradedValue = value.upgraded(newName: modelName)
@@ -31,7 +39,8 @@ final class IndexController {
                 isPublic: isPublic,
                 modelType: modelType,
                 declareVariableProperties: declareVariableProperties,
-                jsonDictionaryName: jsonDictionaryName
+                jsonDictionaryName: jsonDictionaryName,
+                propertyMap: propertyMap
             )
             ouputModel = upgradedValue.swiftStructCode(indentation: indentation, meta: meta)
         } else {
@@ -41,6 +50,7 @@ final class IndexController {
             "jsonString": jsonString,
             "modelName": modelName,
             "jsonDictionaryName": jsonDictionaryName,
+            "propertyMap": propertyMapString,
             "isPublic": isPublic ? "checked" : "",
             "modelType": modelType,
             "isStructSelected": modelType == "struct" ? "selected" : "",
