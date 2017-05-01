bdc75a874b8290c666c73f59a3177fbd4ddf454d
nixzhu <zhuhongxu@gmail.com>
2017-05-01 11:54:15 +0800

    arrayObjectMap

--- Sources/App/Controllers/IndexController.swift
+++ Sources/App/Controllers/IndexController.swift
@@ -17,13 +17,13 @@ final class IndexController {
                 return nil
             }
         }
-        let jsonString = string(forKey: "json_string") ?? "{\n  \"name\": \"NIX\",\n  \"age\": 18, \n  \"detail\": {\n    \"is_dog_lover\": true,\n    \"skills\": [\n      {\n        \"lang\": \"Swift\",\n        \"platform\": \"iOS\"\n      },\n      {\n        \"lang\": \"C\",\n        \"platform\": null\n      }\n    ]\n  }\n}"
+        let jsonString = string(forKey: "json_string") ?? "{\n  \"name\": \"NIX\",\n  \"age\": 18, \n  \"detail\": {\n    \"is_dog_lover\": true,\n    \"skills\": [\n      {\n        \"language\": \"Swift\",\n        \"platform\": \"iOS\"\n      },\n      {\n        \"language\": \"C\",\n        \"platform\": null\n      }\n    ]\n  }\n}"
         let modelName = string(forKey: "model_name") ?? "Model"
         let isPublic = string(forKey: "isPublic") == "on"
         let modelType = string(forKey: "modelType") ?? "struct"
         let declareVariableProperties = string(forKey: "isVariable") == "on"
         let jsonDictionaryName = string(forKey: "json_dictionary_name") ?? "[String: Any]"
-        let propertyMapString = string(forKey: "property_map") ?? "is_dog_lover:likeDogs,lang:language"
+        let propertyMapString = string(forKey: "property_map") ?? "is_dog_lover:likeDogs,skills:workSkills"
         var propertyMap: [String: String] = [:]
         propertyMapString.components(separatedBy: ",").forEach {
             let parts = $0.components(separatedBy: ":")
@@ -31,18 +31,27 @@ final class IndexController {
                 propertyMap[parts[0]] = parts[1]
             }
         }
+        let arrayObjectMapString = string(forKey: "array_object_map") ?? "skills:WorkSkill"
+        var arrayObjectMap: [String: String] = [:]
+        arrayObjectMapString.components(separatedBy: ",").forEach {
+            let parts = $0.components(separatedBy: ":")
+            if parts.count == 2 {
+                arrayObjectMap[parts[0]] = parts[1]
+            }
+        }
         let ouputModel: String
         if let (value, _) = parse(jsonString) {
-            let upgradedValue = value.upgraded(newName: modelName)
+            let upgradedValue = value.upgraded(newName: modelName, arrayObjectNameMap: arrayObjectMap)
             let indentation = Indentation(level: 0, unit: "    ")
             let meta = Meta(
                 isPublic: isPublic,
                 modelType: modelType,
                 declareVariableProperties: declareVariableProperties,
                 jsonDictionaryName: jsonDictionaryName,
-                propertyMap: propertyMap
+                propertyMap: propertyMap,
+                arrayObjectNameMap: arrayObjectMap
             )
-            ouputModel = upgradedValue.swiftStructCode(indentation: indentation, meta: meta)
+            ouputModel = upgradedValue.swiftCode(indentation: indentation, meta: meta)
         } else {
             ouputModel = jsonString.isEmpty ? "" : "Invalid JSON!"
         }
@@ -51,6 +60,7 @@ final class IndexController {
             "modelName": modelName,
             "jsonDictionaryName": jsonDictionaryName,
             "propertyMap": propertyMapString,
+            "arrayObjectMap": arrayObjectMapString,
             "isPublic": isPublic ? "checked" : "",
             "modelType": modelType,
             "isStructSelected": modelType == "struct" ? "selected" : "",
