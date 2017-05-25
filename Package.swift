69b08a5123975d7330bca18322d62c9f96ad0bc7
nixzhu <zhuhongxu@gmail.com>
2017-05-25 10:40:07 +0800

    update Baby

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SharedBaby",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/nixzhu/Baby.git", majorVersion: 0, minor: 14)
+        .Package(url: "https://github.com/nixzhu/Baby.git", majorVersion: 0, minor: 15)
     ],
     exclude: [
         "Config",
